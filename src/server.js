1da6e6c1bf7740ea086f1e6d1b682e6eec438206
i21chardh <richhartranft@gmail.com>
2026-09-23 17:20:40 -0400

    Keep REAPER live displays readable after restart

--- src/server.js
+++ src/server.js
@@ -983,5 +983,5 @@ createServer(async (request, response) => {
   }
 }).listen(port, '127.0.0.1', () => {
   console.log(`Companion Command Builder: http://127.0.0.1:${port}`);
-  writeSystemLog('info', 'server-started', { builderVersion: '0.20.83-beta.1+187', companionTarget: config.companion.version, port, platform: process.platform, node: process.version }).catch(() => {});
+  writeSystemLog('info', 'server-started', { builderVersion: '0.20.84-beta.1+188', companionTarget: config.companion.version, port, platform: process.platform, node: process.version }).catch(() => {});
 });
